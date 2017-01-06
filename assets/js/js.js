0e3a023eb6b892f87441c7fc0a800d5ee39ed0be
Nevin RAK <nevinrak@MacBook-Pro-de-Nevin.local>
2017-01-06 14:14:10 -0600

    Vamo a calmarno

--- assets/js/js.js
+++ assets/js/js.js
@@ -2,4 +2,5 @@ $(document).ready(function() {
     $("#bs-example-navbar-collapse-1 li a[href='#']").click(function(e) {
       e.preventDefault();
     });
+    $("a[href='#'].navbar-brand").click(function(e){ e.preventDefault() });
 });
